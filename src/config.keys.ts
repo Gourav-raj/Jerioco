687d474bde6db3319234c83dc47369917000db92
Gourav-raj <gouravrj9654@gmail.com>
2021-03-17 16:18:53 +0530

    gr

---
+++ src/config.keys.ts
@@ -0,0 +1,8 @@
+const REACT_APP_CLIENT_URL = "https://localhost:3000"
+const REACT_APP_SERVER_URL = "https://localhost:5000"
+const REACT_APP_CONVERGENCE_URL = "https://localhost:8080" 
+
+export const SERVER_URL = process.env.REACT_APP_SERVER_URL! || "http://localhost:5000";
+export const CLIENT_URL = process.env.REACT_APP_CLIENT_URL! || "http://localhost:3000";
+export const CONVERGENCE_URL = process.env.REACT_APP_CONVERGENCE_URL!;
+export const PUBLIC_ROOM = ["public-room1", "public-room2", "public-room3", "public-room4", "public-room5"];
